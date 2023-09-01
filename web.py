29cfe1cd37fb4471dc559d054e42a988716d7188
snskwhsrud <140360824+snskwhsrud@users.noreply.github.com>
2023-09-01 16:03:03 +0900

    init

---
+++ web.py
@@ -0,0 +1,20 @@
+# 프론트 엔드 구성 3요소
+
+# 1.HTML (Hyper Text Markup Language)
+# 웹 페이지의 구조와 내용 (뼈)
+# Hyper Text : 어딘가와 연결되어 있는 텍스트 
+# Markup Language : 태그로 이루어진 언어 
+# <> </> : 태그 
+
+# 2.CSS ( Cascading Style Sheet)
+# 웹 페이지의 모양 디자인 (가죽)
+# 스타일 시트 or 종속형 시트 
+
+
+# 3.Javascript 
+# 웹 페이지의 행동과 응용 프로그램 ( 근육, 지방 )
+# 스크립트 언어로 되어 있다. : 응용 프로그램을 제어하는 역할은 한다.
+
+# <br> : break
+# <hr> : horizontal rule
+# &npsp : non breaking space
