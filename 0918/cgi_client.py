cf54ea65205ef9536c3df5f10671059e1a321fc1
snskwhsrud <140360824+snskwhsrud@users.noreply.github.com>
2023-09-19 13:12:42 +0900

    0919오전 수업

--- 0918/cgi_client.py
+++ 0918/cgi_client.py
@@ -1,16 +1,16 @@
 from urllib.request import urlopen
 from urllib.parse import urlencode
 
-url = "https://127.0.0.1:8888/cgi-bin/cgi_server.py"
+url = "http://127.0.0.1:8888/cgi-bin/script.py"
 
-data = {
-    "name" : "kim",
+data ={
+    "name" : "KIM",
     "email" : "kim@gamil.com",
-    "url" : "httpp://www.naver.com",
+    "url" : "http://www.naver.com",
 }
 
 encData = urlencode(data)
 postData = encData.encode('ascii')
 
-f= urlopen(url, postData)
+f = urlopen(url, postData)
 print(f.read().decode('cp949'))
